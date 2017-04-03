5a109691e6f3bc649c2229b259e58339df63e62a
eronryabets <eronryabets@gmail.com>
2017-04-03 20:54:30 +0300

    defg22ee

--- tst2.py
+++ tst2.py
@@ -1,3 +1,21 @@
-print(type(3), type(2), type(3/2))
-print(3 / 2) # деление с плавающей точкой
-print(3 // 2) # целочисленное деление
+#import - подключение модуля
+# должен отдилятся двумя пустыми строчками
+#import random
+#help(random)
+#print(random.randint(1, 6))
+#module.method - module_name.method_name
+#x = 7
+#x % 6
+#print(x % 6)
+
+#prefix = "Hi, "
+prefix = "Hi"
+name = input ('Please, enter a name')
+#print("Hi ", name)
+#print('Hi, ', name, sep='')
+#print(prefix + name)
+# plus для формирования строк некогда не используется
+print('{},{}'.format(prefix, name))
+#format(prefix, name)) - переменные
+# колво скобок слева - должны соответствовать аргументам (как тут, префикс, нейм)
+
