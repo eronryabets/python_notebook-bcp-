6461b331b51a8e32e1b4a8abf77d1c2863b808ca
eronryabets <eronryabets@gmail.com>
2017-04-05 19:16:26 +0300

    defg22ee

--- bkp1.py
+++ bkp1.py
@@ -2,4 +2,3 @@ hours = float(input("Введите рабочие часы: "))
 rate = float(input("Введите ставку"))
 result = str(hours * rate)
 print("Ответ " + result)
-#sd
---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+def print_hello():
+	print ("hello ", end=" ")
+	print("hello ")
+
+def get_name():
+	name = input("Enter your name: ")
+	print_hello(),
+	print(name)
+
+get_name()
