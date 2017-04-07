52537271bc4333c93d0721bd1cce6b846a998bb9
eronryabets <eronryabets@gmail.com>
2017-04-07 16:32:16 +0300

    asd

--- test.py
+++ test.py
@@ -1,12 +1,40 @@
-def printTwice(bruce):
-  print(bruce, bruce)
-  print(cat)
+#LECTURE #
+"""
+alian = print(type(32))
+alian
+balian = type(32)
+print(balian)
+"""
+"""
+print(id(3))
+alian = 3
+print(id(alian))
+"""
+"""
+print(float(99/100))
+a = 50.0 #class float
+b = 50 #class int
+print(type(a))
+print(type(b))
 
-def catTwice(part1, part2):
-  cat = part1 + part2
-  printTwice(cat)
+print(a + b)
+c = a + b
+print(type(c))
+
+print('word.............................')
+
+print(str(555) + "word") # = print(format(555) + "word")
+#format(555) = str(555) Форматирование (обычно форматирование строки).
+"""
 
 
-chant1 = "Pie Jesu domine, "
-chant2 = "Dona eis requiem."
-catTwice(chant1, chant2)
+"""
+a = 50
+print(type(a)) #int
+f = float(a)
+print(f) #float 50.0
+print(type(f)) #float
+
+print(1 + 2 * float(3) / 4 - 5 - 2.5) #  =  (1 + (2 * float(3) / 4) - 5 - 2.5) = 1 + (1.5) - 5 - 2.5  = -5.0
+"""
+
